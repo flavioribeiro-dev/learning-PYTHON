46717a575c4a1df8c483ca21b6f7a531c1f493a3
fribeiro <flavioxconsult@gmail.com>
2025-06-22 01:00:42 -0300

    adicionado diretorio com atividades práticas (Fac. UNINTER)

---
+++ ATIVIDADES-PRATICAS-UNINTER/4-aula-pratica-1.py
@@ -0,0 +1,12 @@
+# Soma dos 5 primeiros números inteiros e positivos
+soma = 1+2+3+4+5;
+print(soma);
+
+# Média entre 23, 19 e 31:
+media = (23+19+31)/3;
+print( media );
+
+# O número de vezes que 73 cabe em 403:
+
+
+# A sobra quando 403 é dividido por 73:
