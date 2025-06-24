4f02a9e2a50d01512c5d5f5690c8bcc197a9ad38
fribeiro <flavioxconsult@gmail.com>
2025-06-24 11:20:40 -0300

    foram deletados arquivos duplicados - que foram inseridos cada qual em pasta específica

--- 5-condicionais.py
+++
@@ -1,7 +0,0 @@
-x = int(input('Digite um valor inteiro: '));
-y = int( input('Digite um novo valor inteiro: ') );
-
-if(x % 2 == 0):
-    print(f'O número {x} é PAR');
-else:
-    print(f'O número {x} é ÍMPAR');
--- testes-av1.py
+++
@@ -1,6 +0,0 @@
-valor1 = 10;
-_valor1_ = 11;
-área = 100;
-
-print(área);
-123 = ppp;
